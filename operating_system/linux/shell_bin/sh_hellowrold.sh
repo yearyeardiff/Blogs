d57bee299d29fe37a24d6566025b3273e8f69335
张成浩 <zhangchenghao@zhangch.local>
2018-08-09 14:28:20 +0800

    rename 操作系统-》operating system

---
+++ operating_system/linux/shell_bin/sh_hellowrold.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Program:
+#       This program shows "Hello World!" in your screen.
+# History:
+# 2018/08/08	zch	First release
+PATH=${PATH}:~/shell_bin
+export PATH
+echo -e "Hello World! \a \n"
+exit 0
